843f4918192723f7e198999d4fadfc9c4d7d3dbe
Lucas <lucas39lm@hotmail.com>
2023-08-04 17:02:18 -0300

    Product repository, service and resource

---
+++ catalog/src/main/java/com/product/catalog/repositories/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.product.catalog.repositories;
+
+import com.product.catalog.entities.Category;
+import com.product.catalog.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
